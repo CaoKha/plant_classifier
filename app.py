bc06dc2a5df482dd56aa469ee88ccfe44ce62e38
Kha Nguyen Cao <nguyencaokha131995@gmail.com>
2020-10-11 13:49:14 +0200

    first commit

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
 from flask import Flask, render_template, request, url_for, jsonify
 from fastai.vision.all import *
+import re
 from flask_cors import CORS, cross_origin
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
-# app.debug= True  
+
+learn = load_learner('./Models/model.pkl')
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
-learn = load_learner('./Model/model.pkl')
-# classes = learn.data.classes
-# logging.debug('Learnt classes')
+
+
 
 def predict_single(img_file):
     '''function to take image and return prediction'''
-    logging.debug('This function to take image and return prediction')
-
-    prediction = learn.predict(PILImage.create(img_file))
-    probs_list = prediction  # [2].numpy()
-    print("Inside predict_single")
-    logging.debug('Before probs_list')
+    pred, pred_idx, probs = learn.predict(PILImage.create(img_file))
+    probs_list = [pred, probs[pred_idx].tolist()]
     return probs_list
 
-@app.route('/', methods=['POST'])
+
+@app.route('/result', methods=['POST'])
 def predict():
-    if request.method == 'POST':  
-        logging.debug('Before my_prediction')
-        my_prediction = predict_single(request.files['image'])        
-        logging.debug('Before final_pred')
-        final_pred = str(my_prediction[0])
-        logging.debug('After final_pred')
-    return render_template('results.html', prediction=final_pred,
-                           comment='asd')
- 
+    if request.method == 'POST':
+        my_prediction = predict_single(request.files['inputImage'])
+        plant_class = str(my_prediction[0])
+        plant_type = re.search(r'.+?(?=_)',plant_class).group()
+        growth_stage = re.search(r'(?<=_)[\w+.-]+',plant_class).group()
+        probability = str("{:.2f}".format(float(my_prediction[1])*100))
+    return render_template('results.html', prediction = {
+        "plant_type":plant_type,
+        "growth_stage": growth_stage,
+        "probability": probability,
+    }, comment='asd')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
